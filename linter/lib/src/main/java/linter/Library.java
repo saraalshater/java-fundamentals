c6d68f16599783878f08240c0760959c0aae8757
saraalshater <gleave.alshater@gmail.com>
2021-10-02 16:02:04 +0300

    3

--- linter/lib/src/main/java/linter/Library.java
+++ linter/lib/src/main/java/linter/Library.java
@@ -23,29 +23,7 @@ public class Library {
 
     public static void main(String[] args) throws IOException {
 
-        //    int[][] weeklyMonthTemperatures = {
-        //              {66, 64, 58, 65, 71, 57, 60},
-        //              {57, 65, 65, 70, 72, 65, 51},
-        //              {55, 54, 60, 53, 59, 57, 61},
-        //              {65, 56, 55, 52, 55, 62, 57}
-        //      };
-        //       temp(weeklyMonthTemperatures);
-        //       System.out.println(temp(weeklyMonthTemperatures));
-//          System.out.println("-------------------------------------");
-        //       List<String> votes = new ArrayList<>();
-        //       votes.add("Bush");
-        //       votes.add("Bush");
-        //       votes.add("Bush");
-//        votes.add("Hedge");
-        //       votes.add("Shrub");
-        //       votes.add("Bush");
-        //      votes.add("Hedge");
-        //      votes.add("Bush");
-//
-        //       String winner = tally(votes);
 
-        //      System.out.println(winner + " received the most votes!");
-        //      System.out.println("----------------------------------------------------");
         String sepFile = File.separator;
         String basePath = new File("").getCanonicalPath();
 
